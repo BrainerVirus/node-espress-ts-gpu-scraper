aae29200e21cf857034d1e8c6ba27f631fffcc75
Cristhofer Pincetti <cristhoferpincetti@hotmail.cl>
2022-12-14 22:35:05 -0300

    Fix - Memory leak problem has been resolved

--- src/server.ts
+++ src/server.ts
@@ -13,17 +13,23 @@ const url: any =
 const app = express();
 
 app.get("/", (_req, _res) => {
+  scrapping();
   _res.send("Welcome to my webscraper!");
 });
 
-puppeteer
-  .launch()
-  .then((browser) => browser.newPage())
-  .then(async (page) => {
+const scrapping = async () => {
+  try {
+    // Launch a new browser instance
+    const browser = await puppeteer.launch();
+    console.log("this is the scrapping");
+    // Create a new page
+    const page = await browser.newPage();
+
+    // Go to the website
     await page.goto(url);
-    return await page.content();
-  })
-  .then((html) => {
+
+    // Extract the pagination information
+    const html = await page.content();
     const $ = cheerio.load(html);
     const pagination: Page = { lastPageNumber: 0 };
     $(".pagination").each(function (this: Page) {
@@ -32,46 +38,42 @@ puppeteer
       );
       pagination.lastPageNumber = lastPageNumber;
     });
-    return pagination;
-  })
-  .then((pagination) => {
+
+    // Scrape the data from each page
     for (let i: number = 1; i <= pagination.lastPageNumber; i++) {
-      puppeteer
-        .launch()
-        .then((browser) => browser.newPage())
-        .then(async (page) => {
-          await page.goto(`${url}${i === 1 ? "" : `page=${i}&`}`);
-          return await page.content();
-        })
-        .then((html) => {
-          const $ = cheerio.load(html);
-          const gpuArr: GPU[] = [];
-          $(".category-browse-result").each(function (this: GPU) {
-            const img: any = $(this)
-              .find("div > .image-container > a > img")
-              .attr("src");
-            const title: string = $(this)
-              .find(".description-container > dl dd:nth-child(2)")
-              .text();
-            const memory: string = $(this)
-              .find(".description-container > dl dd:nth-child(4)")
-              .text();
-            const coreFrecuency: string = $(this)
-              .find(".description-container > dl dd:nth-child(6)")
-              .text();
-            const bus: string = $(this)
-              .find(".description-container > dl dd:nth-child(8)")
-              .text();
-            const gpu: GPU = { title, img, memory, coreFrecuency, bus };
-            gpuArr.push(gpu);
-          });
-          console.log(gpuArr);
-          console.log(`You have scraped ${gpuArr.length} registries`);
-        })
-        .catch(console.error);
+      // Go to the page
+      await page.goto(`${url}${i === 1 ? "" : `page=${i}&`}`);
+
+      // Extract the data
+      const html = await page.content();
+      const $ = cheerio.load(html);
+      const gpuArr: GPU[] = [];
+      $(".category-browse-result").each(function (this: GPU) {
+        const img: any = $(this)
+          .find("div > .image-container > a > img")
+          .attr("src");
+        const title: string = $(this)
+          .find(".description-container > dl dd:nth-child(2)")
+          .text();
+        const memory: string = $(this)
+          .find(".description-container > dl dd:nth-child(4)")
+          .text();
+        const coreFrecuency: string = $(this)
+          .find(".description-container > dl dd:nth-child(6)")
+          .text();
+        const bus: string = $(this)
+          .find(".description-container > dl dd:nth-child(8)")
+          .text();
+        const gpu: GPU = { title, img, memory, coreFrecuency, bus };
+        gpuArr.push(gpu);
+      });
+      console.log(gpuArr);
+      console.log(`You have scraped ${gpuArr.length} registries`);
     }
-  })
-  .catch(console.error);
+  } catch {
+    console.error;
+  }
+};
 
 const port: number = 8080;
 
